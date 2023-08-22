e0c17d76df45eb0bde5652ef07203708557fc4ad
softmakers-dev <softmakers.dev@gmail.com>
2023-08-22 13:09:44 +0000

    initial commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/softmaker/bumperbot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/softmaker/bumperbot_ws/devel;/home/softmaker/workspace_ros/devel;/opt/ros/noetic'.split(';') if '/home/softmaker/bumperbot_ws/devel;/home/softmaker/workspace_ros/devel;/opt/ros/noetic' != '' else []
